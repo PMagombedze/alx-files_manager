84b4e80ff401f2f4d4f40ff894042c66ee9bcfd7
PMagombedze <percymagoras@outlook.com>
2024-03-18 09:27:21 +0200

    apply semistandard

--- utils/redis.js
+++ utils/redis.js
@@ -2,11 +2,14 @@ import { promisify } from 'util';
 import { createClient } from 'redis';
 
 class RedisClient {
-  constructor () {
+  constructor() {
     this.client = createClient();
     this.isClientConnected = true;
     this.client.on('error', (err) => {
-      console.error('Redis client failed to connect:', err.message || err.toString());
+      console.error(
+        'Redis client failed to connect:',
+        err.message || err.toString()
+      );
       this.isClientConnected = false;
     });
     this.client.on('connect', () => {
@@ -14,20 +17,19 @@ class RedisClient {
     });
   }
 
-  isAlive () {
+  isAlive() {
     return this.isClientConnected;
   }
 
-  async get (key) {
+  async get(key) {
     return promisify(this.client.GET).bind(this.client)(key);
   }
 
-  async set (key, value, duration) {
-    await promisify(this.client.SETEX)
-      .bind(this.client)(key, duration, value);
+  async set(key, value, duration) {
+    await promisify(this.client.SETEX).bind(this.client)(key, duration, value);
   }
 
-  async del (key) {
+  async del(key) {
     await promisify(this.client.DEL).bind(this.client)(key);
   }
 }
