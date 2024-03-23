a5ffd6a57b42b36272a79fca0b2f8024c8fdd397
PMagombedze <percymagoras@outlook.com>
2024-03-23 11:46:30 +0200

    redis eslint

--- utils/redis.js
+++ utils/redis.js
@@ -2,7 +2,7 @@ import { promisify } from 'util';
 import { createClient } from 'redis';
 
 class RedisClient {
-  constructor() {
+  constructor () {
     this.client = createClient();
     this.isClientConnected = true;
     this.client.on('error', (err) => {
@@ -17,19 +17,19 @@ class RedisClient {
     });
   }
 
-  isAlive() {
+  isAlive () {
     return this.isClientConnected;
   }
 
-  async get(key) {
+  async get (key) {
     return promisify(this.client.GET).bind(this.client)(key);
   }
 
-  async set(key, value, duration) {
+  async set (key, value, duration) {
     await promisify(this.client.SETEX).bind(this.client)(key, duration, value);
   }
 
-  async del(key) {
+  async del (key) {
     await promisify(this.client.DEL).bind(this.client)(key);
   }
 }
