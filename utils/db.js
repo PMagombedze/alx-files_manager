206dd493fb947c8beaa023ec83df7972b43ceae6
PMagombedze <percymagoras@outlook.com>
2024-03-25 20:54:47 +0200

    redis eslint

--- utils/db.js
+++ utils/db.js
@@ -5,10 +5,14 @@ const DB_PORT = process.env.DB_PORT || 27017;
 const DB_DATABASE = process.env.DB_DATABASE || 'files_manager';
 const url = `mongodb://${DB_HOST}:${DB_PORT}`;
 
+/**
+ * Class for performing operations with Mongo service
+ */
 class DBClient {
   constructor() {
     MongoClient.connect(url, { useUnifiedTopology: true }, (err, client) => {
       if (!err) {
+        // console.log('Connected successfully to server');
         this.db = client.db(DB_DATABASE);
         this.usersCollection = this.db.collection('users');
         this.filesCollection = this.db.collection('files');
@@ -19,15 +23,27 @@ class DBClient {
     });
   }
 
+  /**
+   * Checks if connection to Redis is Alive
+   * @return {boolean} true if connection alive or false if not
+   */
   isAlive() {
     return Boolean(this.db);
   }
 
+  /**
+   * Returns the number of documents in the collection users
+   * @return {number} amount of users
+   */
   async nbUsers() {
     const numberOfUsers = this.usersCollection.countDocuments();
     return numberOfUsers;
   }
 
+  /**
+   * Returns the number of documents in the collection files
+   * @return {number} amount of files
+   */
   async nbFiles() {
     const numberOfFiles = this.filesCollection.countDocuments();
     return numberOfFiles;
